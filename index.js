9098c6494b7a09f5be0553b8f3e454d9655aeef0
tina05412 <tinaaaaa0512@gmail.com>
2025-06-16 17:04:05 +0530

    step1- intialized project added dependencies

---
+++ index.js
@@ -0,0 +1,28 @@
+const jwt=require("jsonwebtoken")
+const express=require("express")
+const mongoose=require("mongoose")
+
+const app=express()
+
+app.post('/signup',async function (req,res) {
+    
+});
+
+
+app.get('/login',async function (req,res) {
+    
+});
+
+
+app.get('/purchase',async function (req,res) {
+    
+});
+
+
+app.get('/courses',async function (req,res) {
+    
+});
+
+app.get('/PurchasedCourses',async function (req,res) {
+    
+});
